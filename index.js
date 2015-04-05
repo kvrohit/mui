dca6d02f0cb9add312e46bfa3b34e77071e81dc2
Rohit K Viswanath <kvrohit@gmail.com>
2015-04-05 17:05:09 +0530

    Basic router setup

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ var DefaultRoute  = Router.DefaultRoute;
 var Link          = Router.Link;
 var RouteHandler  = Router.RouteHandler;
 
+var About         = require('./src/jsx/about.jsx');
+
 var App = React.createClass({
   render: function() {
     return (
       <div className="main">
         <h1>Hello World</h1>
+        <RouteHandler/>
       </div>
     );
   }
@@ -18,6 +21,7 @@ var App = React.createClass({
 
 var routes = (
   <Route handler={App} path="/">
+    <Route name="about" handler={About} path="/about" />
   </Route>
 );
 
